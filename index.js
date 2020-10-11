7be94d8d11d23d1e4af3ccc4f0a91abd058d2ace
Kirk Tolliver <kirktolliver28@gmail.com>
2020-10-11 17:36:02 -0500

    Initial commit server running

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const express  = require("express")
+const server = express()
+
+server.use(express.json())
+
+server.listen(8080, () => {
+    console.log("server started")
+})
