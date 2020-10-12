746013531e7d58daf4d8a006f5673adfb57e9a6f
Kirk Tolliver <kirktolliver28@gmail.com>
2020-10-11 19:31:15 -0500

    Get operation complete for projects

--- actions/action-routes.js
+++ actions/action-routes.js
@@ -1,4 +1,11 @@
 const express = require("express")
+const db = require("../data/helpers/actionModel")
+
+
 const router = express.Router()
 
+
+router.get("/", (req, res) => {
+    
+})
 module.exports = router
--- projects/project-routes.js
+++ projects/project-routes.js
@@ -1,4 +1,32 @@
 const express = require("express")
+const project = require("../data/helpers/projectModel")
 const router = express.Router()
+
+router.get("/", (req, res) => {
+    project.get()
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "Error retrieving the database",
+            });
+        })
+});
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id 
+    project.get(id)
+        .then(project => {
+            res.status(200).json(project);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: "Error retrieving the database",
+            });
+        })
+});
 
 module.exports = router
